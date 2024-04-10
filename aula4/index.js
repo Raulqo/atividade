759c91558cbfda0d7b31cbb2bd7a71cc7905d65e
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:56:57 -0300

    teste variaveis

--- aula4/index.js
+++ aula4/index.js
@@ -1,1 +1,10 @@
-console.log ("hello world")
+console.log ("hello world") 
+const nome = "Raul"
+const idade = 15
+console.log("meu nome é ",nome," e tenho ",idade," de idade"
+)
+//teste
+const idadenumero = 23
+const idadetexto = idadenumero.toString()
+console.log(typeof idadenumero)
+console.log(typeof idadetexto)
