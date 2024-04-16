1fbff01b945e048f43ed1a43ba346df22d5b9bde
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 11:58:07 -0300

    exercicios aula 16_04

---
+++ aula 16_04/index.js
@@ -0,0 +1,89 @@
+/*let comparacao
+resultado ="1" === "2"
+alert (resultado)
+
+resultado = "1" === "1"
+alert (resultado)
+
+resultado = "2" === "2"
+alert (resultado)
+
+resultado = "2" === "2"
+alert (resultado)*/
+
+//leitura de dados informados pelo usuario
+/*primeironumero = prompt("digite o seu primeiro numero:")
+segundonumero = prompt("Digite o seu segundo numero:")
+
+//teste de comparação
+resultado = primeironumero === segundonumero
+
+//mostra resultado na tela
+alert(resultado)
+
+resultado = primeironumero !== segundonumero
+alert(resultado)
+
+resultado = primeironumero > segundonumero
+alert(resultado)
+
+resultado = primeironumero < segundonumero
+alert(resultado)*/
+
+/*//declaração de variaveis
+let a
+let b
+let c
+let resultado
+
+//leitura de dados informados pelo usuario
+
+a = true 
+b = false 
+c = true
+
+//1. realize a operação: a && b
+resultado = a && b
+alert(resultado)
+//2. realize a operação: b && c
+resultado = b && c
+alert(resultado)
+//3. realize a operação: a && c
+resultado = a && c
+alert(resultado)
+//4. realize a operação: a && b && c
+resultado = a && b && c
+alert(resultado)
+//1. realize a operação: a || b
+resultado = a || b
+alert(resultado)
+//2. realize a operação: b || c
+resultado = b || c
+alert(resultado)
+//3. realize a operação: a || c
+resultado = a || c
+alert(resultado)
+//4. realize a operação: a || b || c
+resultado = a || b || c
+alert(resultado) */
+
+//declara variaveis
+let nome 
+let anonascimento 
+let anoatual 
+let idade 
+let maiordeidade
+let idade2050
+//calcula variaveis
+nome = prompt ("Digite seu nome:")
+anonascimento = prompt ("Digite seu ano de nascimento:")
+anoatual = prompt ("digite o ano atual:")
+//
+idade = anoatual - anonascimento
+maiordeidade = idade >= 18
+idade2050 = 2050 - anonascimento
+//
+alert(nome)
+alert(idade)
+alert(maiordeidade)
+alert(idade2050)
