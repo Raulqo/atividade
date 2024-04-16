84d9d3e470969a1ff03214ffb64fc2998351990b
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 10:37:49 -0300

    aula dia 15_04

---
+++ aula 15_04/index.js
@@ -0,0 +1,49 @@
+/*let resultado
+
+resultado = 3 + 4
+alert(resultado)
+
+resultado = (3+5)/2
+alert(resultado)
+
+resultado = 3+5/2
+alert(resultado)
+
+resultado = (4-5)*-1
+alert (resultado)
+
+resultado = 234 % 5
+alert (resultado)*/
+
+/*let comparacao
+resultado ="1" === "2"
+alert (resultado)
+
+resultado = "1" === "1"
+alert (resultado)
+
+resultado = "2" === "2"
+alert (resultado)
+
+resultado = "2" === "2"
+alert (resultado)*/
+
+//leitura de dados informados pelo usuario
+primeironumero = prompt("digite o seu primeiro numero:")
+segundonumero = prompt("Digite o seu segundo numero:")
+
+//teste de comparação
+resultado = primeironumero === segundonumero
+
+//mostra resultado na tela
+alert(resultado)
+
+resultado = primeironumero !== segundonumero
+alert(resultado)
+
+resultado = primeironumero > segundonumero
+alert(resultado)
+
+resultado = primeironumero < segundonumero
+alert(resultado)
+
