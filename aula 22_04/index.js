dc4b18d5de6a0358209a8a303fc28f6928bf1fc3
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-22 11:51:34 -0300

    atividades aula 22_04

---
+++ aula 22_04/index.js
@@ -0,0 +1,9 @@
+/*let nome = prompt("digite seu nome:")
+let email = prompt("digite seu email")
+alert(` O E-mail ${email} foi cadastrado com sucesso. Seja bem-vindo(a), ${nome}`)*/
+
+//let comidafavUsuario = prompt("digite uma comida favorita:")
+console.log(`Essas são minhas comidas favoritas`)
+let comidasFavoritas = [" Pizza", " strogonnof", " Sopa", " Pudim", " Churrasco"]
+console.log (comidasFavoritas[0] + "\n" + comidasFavoritas[1] + "\n" + comidasFavoritas[2] + "\n" + comidasFavoritas[3] + "\n" + comidasFavoritas[4])
+//let comidafavUsuario = prompt("digite uma comida favorita:")
