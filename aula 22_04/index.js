88bf632dde2b42777e1c9b455015de891fefabf7
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 11:56:21 -0300

    atividades aula 23_05

--- aula 22_04/index.js
+++ aula 22_04/index.js
@@ -6,4 +6,4 @@ alert(` O E-mail ${email} foi cadastrado com sucesso. Seja bem-vindo(a), ${nome}
 console.log(`Essas são minhas comidas favoritas`)
 let comidasFavoritas = [" Pizza", " strogonnof", " Sopa", " Pudim", " Churrasco"]
 console.log (comidasFavoritas[0] + "\n" + comidasFavoritas[1] + "\n" + comidasFavoritas[2] + "\n" + comidasFavoritas[3] + "\n" + comidasFavoritas[4])
-//let comidafavUsuario = prompt("digite uma comida favorita:")
+
---
+++ aula 23_04/index.js
@@ -0,0 +1,92 @@
+//Exercício 1
+
+/*let num1 = Number(prompt("digite um numero:"))
+let num2 = Number(prompt("digite outro numero:"))
+if (num1 == num2){
+   alert (`sucesso!!!`)
+} else {
+   alert(`Não teve sucesso!`)
+}*/
+
+//EXERCÍCIO 2
+/*let num1 = Number(prompt("digite um numero:"))
+let num2 = Number(prompt("digite outro numero:"))
+if (num1 == num2){
+   alert (`Numeros iguais!!!`)
+} else {
+   alert(`Numeros diferentes!!!`)
+}*/
+
+//EXERCÍCIO 3
+/*let num1 = Number(prompt("digite um numero:"))
+let num2 = Number(prompt("digite outro numero:"))
+if (num1 == num2){
+   alert (`Numeros iguais!!!`)
+} else if (`Numero menor!!!`)  {
+   alert(`Numero menor!!!`)
+} else  {
+    alert(`Numero maior!!!`)
+}*/
+ 
+//jeito correto
+/*let num1 = Number(prompt("digite um numero:"))
+let num2 = Number(prompt("digite outro numero:"))
+if (num1 > num2){
+    alert('Numero maior!!!')
+} else if (num1 < num2) {
+    alert('Numero menor!!!')
+} else if (num1 == num2){
+    alert('Numeros iguais!!!')
+}*/
+
+//EXERCÍCIO 4
+
+/*let pokemon
+pokemon = prompt("Digite seu primeiro pokemon")
+switch(pokemon){
+    case 'Bulbasauro':
+     alert(`planta e veneno!!!`)
+    break
+    case(`Squirtle`):
+     alert("agua!!!")
+    break
+    case 'Charmander':
+     alert ("fogo!!!")
+    break
+    default:
+    alert("Não é um pokemon inicial!!!")
+}*/
+
+//EXERCÍCIO 5
+/*let cond1 = prompt("terminou ensino medio?")
+let cond2 = prompt("voce tem mais de 18 anos?")
+let cond3 = prompt("voce está cursando outra faculdade?")
+
+if ((cond1 == "sim") && (cond2 == "sim") && (cond3 == "não")){
+    alert('Você pode cursar a faculdade!!!')
+} else {
+    alert('Você não pode cursar a faculdade!!!')
+}*/
+
+//exercicios escrita de codigo
+/*let idade = Number(prompt("Quantos anos você tem?"))
+let maiorIdade = Number("18")
+if ( idade >= maiorIdade){
+    alert("Você pode dirigir!!!")
+} else if (idade <= maiorIdade) {
+    alert("Você não pode dirigir!!")
+}*/
+
+let turno1 = prompt("Você estuda de manhã?")
+let turno2 = prompt("Você estuda de tarde?")
+let turno3 = prompt("Você estuda de noite?")
+if ("turno1 == sim") ("turno2 == não") ("turno3 == não"){
+    alert("Bom dia!!!") 
+} else if ("turno1 == Não") ("turno2 == Sim") ("turno3 == não") {
+    alert("Boa Tarde!!!")
+} else if ("turno1 == Não") ("turno2 == não") ("turno3 == Sim") {
+    alert("boa noite!!!")
+}
+
+
+
