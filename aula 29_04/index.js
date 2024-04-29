c5d285d9d708215c04e0c48245ead2d92d3f2923
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-29 11:50:58 -0300

    exercicios aula 29_04

---
+++ aula 29_04/index.js
@@ -0,0 +1,27 @@
+//atividade 1
+/*function imprimirOlaMundo(){
+    console.log("Olá Mundo!!!")
+}
+imprimirOlaMundo()*/
+
+//atividade 2
+/*function imprimirNome(nome){
+    alert (`Olá ${nome}`)
+}
+let nomeUser = " "
+nomeUser = prompt ("digite o seu nome:")
+imprimirNome(nomeUser)
+nomeUser = prompt ("digite o seu nome:")
+imprimirNome(nomeUser)
+nomeUser = prompt ("digite o seu nome:")
+imprimirNome(nomeUser)*/
+
+function somaNum (num1 , num2) {
+    let soma = num1 + num2
+    return soma
+}
+let numero1 = 3
+let numero2 = 12
+let resultado = 0 
+resultado = somaNum(numero1, numero2)
+alert(resultado)
